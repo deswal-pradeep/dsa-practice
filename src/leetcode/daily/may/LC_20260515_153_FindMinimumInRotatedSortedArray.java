1dff71242f8e0563e6916741946de1821a0f7946
deswal-pradeep <deswal.pradeep16@gmail.com>
2026-05-15 10:23:14 +0530

    Daily Challenge 15th May

---
+++ src/leetcode/daily/may/LC_20260515_153_FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,25 @@
+package leetcode.daily.may;
+
+//https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/?envType=daily-question&envId=2026-05-15
+public class LC_20260515_153_FindMinimumInRotatedSortedArray {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while(low < high){
+            int mid = (low + high) >> 1;
+            if(nums[mid] < nums[high]){
+                //means right half is sorted but ans might be there at mid as well
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return nums[high];
+    }
+
+    static void main() {
+        int min = new LC_20260515_153_FindMinimumInRotatedSortedArray()
+                .findMin(new int[]{4, 5, 6, 7, 0, 1, 2});
+        System.out.println(min);
+    }
+}
