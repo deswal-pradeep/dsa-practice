e66bc689143a2d0127be24c2d8b1ceaa3c84bc75
deswal-pradeep <deswal.pradeep16@gmail.com>
2026-05-18 10:58:41 +0530

    18th may daily challenge

---
+++ src/leetcode/daily/may/LC_20260518_1345_JumpGameIV.java
@@ -0,0 +1,45 @@
+package leetcode.daily.may;
+
+import java.util.*;
+
+public class LC_20260518_1345_JumpGameIV {
+    public int minJumps(int[] arr) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            List<Integer> list = map.computeIfAbsent(arr[i], k -> {return new ArrayList<>();});
+            list.add(i);
+        }
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[] visited = new boolean[arr.length];
+        visited[0] = true;
+        queue.add(new int[]{0, 0});
+        while(!queue.isEmpty()){
+            int[] node = queue.poll();
+            int index = node[0];
+            int steps = node[1];
+            visited[index] = true;
+            if(index == arr.length-1)
+                return node[1];
+            List<Integer> list = map.get(arr[index]);
+            for(int idx : list){
+                if(idx != index && !visited[idx])
+                    queue.add(new int[]{idx, steps+1});
+            }
+            list.clear();
+            if(index+1 < arr.length && !visited[index+1]){
+                queue.add(new int[]{index+1, steps+1});
+            }
+            if(index-1 > 0 && !visited[index-1]){
+                queue.add(new int[]{index-1, steps+1});
+            }
+        }
+        return -1;
+    }
+
+    static void main() {
+        int ans = new LC_20260518_1345_JumpGameIV()
+                .minJumps(new int[]{100,-23,-23,404,100,23,23,23,3,404});
+        System.out.println(ans);
+    }
+
+}
