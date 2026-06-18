a0f05615a476e81b00daf9b55d6314d32702afff
deswal-pradeep <deswal.pradeep16@gmail.com>
2026-06-18 09:56:44 +0530

    daily challenge 18th june

---
+++ src/leetcode/daily/y2026/jun/LC_20260618_1344_AngleBetweenHandsOfAClock.java
@@ -0,0 +1,20 @@
+package leetcode.daily.y2026.jun;
+
+public class LC_20260618_1344_AngleBetweenHandsOfAClock {
+    public double angleClock(int hour, int minutes) {
+        double hourAngle = (hour + (minutes / 60.0)) * 30.0;
+        hourAngle = hourAngle >= 360 ? hourAngle - 360 : hourAngle;
+        double minuteAngle = (minutes * 6.0);
+        double ans = hourAngle > minuteAngle
+                ? hourAngle -  minuteAngle
+                : minuteAngle - hourAngle;
+        ans = Math.min(360 - ans, ans);
+        return ans;
+    }
+
+    static void main() {
+        double angle = new LC_20260618_1344_AngleBetweenHandsOfAClock()
+                .angleClock(12, 30);
+        System.out.println(angle);
+    }
+}
