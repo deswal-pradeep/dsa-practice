6c0772b4c2f29b46fdc77f2b46246f1c90639f33
deswal-pradeep <deswal.pradeep16@gmail.com>
2026-06-19 10:19:52 +0530

    19th june daily challenge

---
+++ src/leetcode/daily/y2026/jun/LC_20260619_1732_FindTheHighestAltitude.java
@@ -0,0 +1,20 @@
+package leetcode.daily.y2026.jun;
+
+//https://leetcode.com/problems/find-the-highest-altitude/?envType=daily-question&envId=2026-06-19
+public class LC_20260619_1732_FindTheHighestAltitude {
+    public int largestAltitude(int[] gain) {
+        int alt = 0;
+        int maxi = 0;
+        for(int g : gain){
+            alt += g;
+            maxi = Math.max(alt, maxi);
+        }
+        return maxi;
+    }
+
+    static void main() {
+        int ans = new LC_20260619_1732_FindTheHighestAltitude()
+                .largestAltitude(new int[]{-4, -3, -2, -1, 4, 3, 2});
+        System.out.println(ans);
+    }
+}
