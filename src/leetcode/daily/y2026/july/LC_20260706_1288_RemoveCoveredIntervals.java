0d16760851caa0c45ea86efbe38e36af44c987f6
deswal-pradeep <deswal.pradeep16@gmail.com>
2026-07-06 10:07:53 +0530

    daily challenge 6th jul

---
+++ src/leetcode/daily/y2026/july/LC_20260706_1288_RemoveCoveredIntervals.java
@@ -0,0 +1,56 @@
+package leetcode.daily.y2026.july;
+
+import java.util.Arrays;
+
+public class LC_20260706_1288_RemoveCoveredIntervals {
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> {
+            int compare1 = Integer.compare(a[0], b[0]);
+            if(compare1 == 0)
+                return Integer.compare(a[1], b[1]) * -1;
+            return compare1;
+        });
+
+        int maxEndSeenSoFar = 0;
+        int count = 0;
+
+        for (int[] interval: intervals) {
+            if(interval[1] > maxEndSeenSoFar) {
+                maxEndSeenSoFar = interval[1];
+                count++;
+            }
+        }
+        return count;
+    }
+    public int removeCoveredIntervals_1(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> {
+            int compare1 = Integer.compare(a[0], b[0]);
+            if(compare1 == 0)
+                return Integer.compare(a[1], b[1]) * -1;
+            return compare1;
+        });
+        boolean[] covered = new boolean[intervals.length];
+        for(int i = 0; i < intervals.length; i++){
+            for(int j = i+1; j < intervals.length; j++){
+                int[] first = intervals[i];
+                int[] second = intervals[j];
+                if(second[0] > first[1]){
+                    break;
+                }
+                if(first[0] <= second[0] && first[1] >= second[1])
+                    covered[j] = true;
+            }
+        }
+        int count = 0;
+        for(boolean b : covered){
+            if(!b) count++;
+        }
+        return count;
+    }
+
+    static void main() {
+        int ans = new LC_20260706_1288_RemoveCoveredIntervals()
+                .removeCoveredIntervals(new int[][]{{1,2},{1,4},{3,4}});
+        System.out.println(ans);
+    }
+}
