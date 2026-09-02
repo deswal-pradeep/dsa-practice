0aaf6bb118b796508dff6482d05d0c614516e8a9
deswal-pradeep <deswal.pradeep16@gmail.com>
2026-09-02 19:20:00 +0530

    daily challenge 2nd sept

---
+++ src/leetcode/daily/y2026/m09/LC_02_ConstructUniformParityArrayI.java
@@ -0,0 +1,8 @@
+package leetcode.daily.y2026.m09;
+
+//https://leetcode.com/problems/construct-uniform-parity-array-i/?envType=daily-question&envId=2026-09-02
+public class LC_02_ConstructUniformParityArrayI {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
