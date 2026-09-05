3f1f391297cb3de62c1de5df07c3c5ba0e006fda
deswal-pradeep <deswal.pradeep16@gmail.com>
2026-09-05 10:48:32 +0530

    daily challenge 5th sep

---
+++ src/leetcode/daily/y2026/m09/LC_05_3904_SmallestStableIndexII.java
@@ -0,0 +1,24 @@
+package leetcode.daily.y2026.m09;
+
+//https://leetcode.com/problems/smallest-stable-index-ii/?envType=daily-question&envId=2026-09-05
+public class LC_05_3904_SmallestStableIndexII {
+    public int firstStableIndex(int[] nums, int k) {
+        int[] suffixMin = new int[nums.length];
+        suffixMin[nums.length-1] = nums[nums.length-1];
+        for(int i = nums.length-2; i >= 0; i--){
+            suffixMin[i] = Math.min(suffixMin[i+1], nums[i]);
+        }
+        int maxi = -(int)1e9;
+        for(int i = 0; i < nums.length; i++){
+            maxi = Math.max(maxi, nums[i]);
+            if(maxi - suffixMin[i] <= k){
+                return i;
+            }
+        }
+        return -1;
+    }
+    static void main() {
+        int ans = new LC_04_3903_SmallestStableIndexI().firstStableIndex(new int[]{5, 0, 1, 4}, 3);
+        System.out.println(ans);
+    }
+}
