fd8cf627de30f533fe956a3d7df4f4e6b2627396
deswal-pradeep <deswal.pradeep16@gmail.com>
2026-09-22 00:09:06 +0530

    daily challenge 22nd sept

---
+++ src/leetcode/daily/y2026/m09/LC_21_3524_FindXValueOfArrayI.java
@@ -0,0 +1,32 @@
+package leetcode.daily.y2026.m09;
+
+import java.util.Arrays;
+
+public class LC_21_3524_FindXValueOfArrayI {
+    public long[] resultArray(int[] nums, int k) {
+        int n = nums.length;
+        int[][] dp = new int[n+1][k];
+
+        for(int i = 1; i <= n; i++){
+            dp[i][nums[i-1]%k] = 1;
+            for(int j = 0; j < k; j++){
+                int cnt = dp[i-1][j];
+                int newR = (j + nums[i-1])%k;
+                dp[i][newR] += cnt;
+            }
+        }
+        long[] ans = new long[k];
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j < k; j++){
+                ans[j] += dp[i][j];
+            }
+        }
+        return ans;
+    }
+
+    static void main() {
+        long[] longs = new LC_21_3524_FindXValueOfArrayI()
+                .resultArray(new int[]{1, 2, 3, 4, 5}, 3);
+        System.out.println(Arrays.toString(longs));
+    }
+}
